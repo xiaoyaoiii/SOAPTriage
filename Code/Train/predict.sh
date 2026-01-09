c59d04a1d507a921300bda1b1c649d1aec7e588e
xiaoyaoiii <2118286420@qq.com>
2026-01-09 15:40:07 +0800

    Update predict.sh

--- Code/Train/predict.sh
+++ Code/Train/predict.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
-python your_script_dir/bert/pre.py \
+python your_script_dir/pre.py \
   --test_files \
     your_output_dir/triage_embed_soap_s.jsonl \
     your_output_dir/triage_embed_soap_o.jsonl \
@@ -8,5 +8,5 @@ python your_script_dir/bert/pre.py \
   --stage1_ckpt your_ckpt_dir/stage1.pt \
   --stage2_high_ckpt your_ckpt_dir/stage2_high.pt \
   --stage2_low_ckpt your_ckpt_dir/stage2_low.pt \
-  --output_path your_output_dir/pred_with_weights.jsonl \
+  --output_path your_output_dir/pred.jsonl \
   --save_named_weights
